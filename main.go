f939b939628e9b3bd22fdc7b916385b01fb89d82
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2025-01-19 17:48:17 -0800

    Update main with bytes to send

--- main.go
+++ main.go
@@ -1,63 +1,131 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"log"
 	"net"
-
-	"github.com/google/go-tpm/tpm2"
+	"os"
+	"time"
 )
 
-var tpmAddress = flag.String("tpm-address", "tpm-simulator:2321", "Address of the TPM simulator (host:port)")
-
-// ConnTransport wraps net.Conn to implement the transport.TPM interface
-type ConnTransport struct {
-	conn net.Conn
+// TPM represents a connection to a TPM simulator.
+type TPM struct {
+	transport *net.TCPConn
 }
 
-// Send sends data to the TPM and receives a response.
-func (c *ConnTransport) Send(data []byte) ([]byte, error) {
-	if _, err := c.conn.Write(data); err != nil {
-		return nil, fmt.Errorf("failed to send data to TPM: %v", err)
-	}
-
-	resp := make([]byte, 4096)
-	n, err := c.conn.Read(resp)
+// Send implements the TPM interface.
+func (t *TPM) Send(input []byte) ([]byte, error) {
+	fmt.Println("Sending bytes to TPM.")
+	_, err := t.transport.Write(input)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read data from TPM: %v", err)
+		fmt.Println("Write to server failed:", err.Error())
+		return nil, err
 	}
 
-	return resp[:n], nil
+	reply := make([]byte, 1024)
+
+	// Set a timeout for the write and read operations
+	timeout := 5 * time.Second // Adjust the timeout duration as needed
+
+	// Set the deadline for reading
+	err = t.transport.SetReadDeadline(time.Now().Add(timeout))
+	if err != nil {
+		fmt.Println("SetReadDeadline failed:", err.Error())
+		return nil, err
+	}
+
+	fmt.Println("Reading response from TPM.")
+	n, err := t.transport.Read(reply)
+	if err != nil {
+		fmt.Println("Read from server failed:", err.Error())
+		return nil, err
+	}
+
+	// // Check how much data was actually read
+	fmt.Printf("Read %d bytes\n", n)
+
+	return reply, nil
 }
 
-func (c *ConnTransport) Close() error {
-	return c.conn.Close()
+// Close implements the TPM interface.
+func (t *TPM) Close() error {
+	return t.transport.Close()
 }
 
 func main() {
-	flag.Parse()
-
-	// Connect to the TPM simulator over TCP
-	conn, err := net.Dial("tcp", *tpmAddress)
+	var err error
+	servAddr := "tpm-simulator:2321"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
 	if err != nil {
-		log.Fatalf("Failed to connect to TPM simulator: %v", err)
-	}
-	defer conn.Close()
-	fmt.Println("Connected to TPM simulator!")
-
-	// Wrap the connection in a ConnTransport to meet the transport.TPM interface
-	tpmTransport := &ConnTransport{conn: conn}
-
-	// Create a GetRandom command for 16 bytes
-	cmd := tpm2.GetRandom{BytesRequested: 16}
-
-	// Execute the command
-	resp, err := cmd.Execute(tpmTransport)
-	if err != nil {
-		log.Fatalf("Failed to execute GetRandom command: %v", err)
+		fmt.Println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
 	}
 
-	// Print the random bytes from TPM
-	fmt.Printf("Random bytes from TPM: %x\n", resp.RandomBytes)
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		fmt.Println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	// var thetpm transport.TPMCloser
+	thetpm := &TPM{
+		transport: conn,
+	}
+	defer thetpm.Close()
+
+	var output []byte
+	var message []byte
+
+	message = []byte{
+		0, 0, 0, 0x0F, // TPM_REMOTE_HANDSHAKE
+		0, 0, 0, 0x04, // Client Version
+	}
+
+	fmt.Printf("Sending handshake. Message: %x\n", message)
+	output, err = thetpm.Send(message)
+	if err != nil {
+		fmt.Println("Remote handshake failed:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Remote Handshake successful. Output: %x ... %x\n", output[:32], output[len(output)-32:])
+
+	message = []byte{
+		0, 0, 0, 0x08, // TPM_SEND_COMMAND
+		0, 0, 0, 0x0C, // length
+		0, 0, 0x01, 0x7B, // TPM_CC_GetRandom
+		0, 0x08, // Command parameter - num random bytes to generate
+	}
+
+	output, err = thetpm.Send(message)
+	fmt.Printf("Sending get random. Message: %x\n", message)
+	if err != nil {
+		fmt.Println("Get random command failed:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Get random successful. Output: %x ... %x\n", output[:32], output[len(output)-32:])
+
+	message = []byte{
+		0, 0, 0, 0x15, // TPM_STOP
+	}
+	fmt.Printf("Sending stop. Message: %x\n", message)
+	output, err = thetpm.Send(message)
+	if err != nil {
+		fmt.Println("Stop failed:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Stop successful. Output: %x ... %x\n", output[:32], output[len(output)-32:])
+
+	// Goal is to have this implementation for each command
+	// so that we can call "Execute" and have the TCP
+	// communication happen.
+
+	// grc := GetRandom{
+	// 	BytesRequested: 16,
+	// }
+
+	// var out *GetRandomResponse
+	// if out, err = grc.Execute(thetpm); err != nil {
+	// 	fmt.Printf("GetRandom failed: %v", err)
+	// 	os.Exit(1)
+	// }
+	// fmt.Printf("Output: %x", out.RandomBytes)
 }
